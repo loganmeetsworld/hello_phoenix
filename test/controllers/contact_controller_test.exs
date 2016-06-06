2c3cc44f7754e6e52d10efec0994c66583a7cb51
Logan McDonald <loganmcdona11@gmail.com>
2016-06-06 14:58:28 -0700

    adding routes for api

--- test/controllers/contact_controller_test.exs
+++ test/controllers/contact_controller_test.exs
@@ -15,7 +15,7 @@ defmodule HelloPhoenix.ContactControllerTest do
     end
   end
 
-  test "/index returns list of contacts" do
+  test "/index returns a list of contacts" do
     contacts_as_json =
       %Contact{name: "Gumbo", phone: "(801) 555-5555"}
       |> Repo.insert
@@ -24,8 +24,8 @@ defmodule HelloPhoenix.ContactControllerTest do
 
     response = conn(:get, "/api/contacts") |> send_request
 
-    assert response.status = 200
-    assert response.resp_body = contacts_as_json
+    assert response.status == 200
+    assert response.resp_body == contacts_as_json
   end
 
   defp send_request(conn) do
